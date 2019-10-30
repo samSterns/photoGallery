ade2dc81781b30f8e4098c47a2c837b7766da7d9
samSterns <sam.g.sterns@gmail.com>
2019-10-29 18:09:38 -0700

    not finished images render

--- home/app.js
+++ home/app.js
@@ -3,6 +3,7 @@ import htmlToDOM from '../util/html-to-DOM.js';
 import renderHornyAnimal from './render-horny-animal.js';
 
 const list = document.querySelector('.horny-animal');
+const imageTypeFilter = document.querySelector('filter-choices');
 
 images.forEach(image => {
 
@@ -12,3 +13,31 @@ images.forEach(image => {
 
     list.appendChild(dom);
 });
+
+
+imageTypeFilter.addEventListener('change', () => {
+
+    const filterString = imageTypeFilter.value;
+    let filteredImages = null;
+
+    if (!filterString) {
+        filteredImages = images;
+    } else {
+        filteredImages = images.filter(image => {
+            return image.keyword === filterString; 
+        });
+    }
+    render(filteredImages);
+});
+
+render(images);
+function render(imagesToRender) {
+    while (URL.lastElementChild){
+        ul.lastElementChild.remove();
+    }
+    imagesToRender.forEach(image => {
+        const html = renderHornyAnimal(image);
+        const dom = htmlToDOM(html);
+        ul.appendChild(dom);
+    });
+}
