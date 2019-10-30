5c6a72fb08591efa122e1c6777cd42ce2d39bf77
samSterns <sam.g.sterns@gmail.com>
2019-10-30 09:53:53 -0700

    filter is working

--- home/app.js
+++ home/app.js
@@ -3,7 +3,18 @@ import htmlToDOM from '../util/html-to-DOM.js';
 import renderHornyAnimal from './render-horny-animal.js';
 
 const list = document.querySelector('.horny-animal');
-const imageTypeFilter = document.querySelector('filter-choices');
+const imageTypeFilter = document.querySelector('.filter-choices');
+
+function render(imagesToRender) {
+    while (list.lastElementChild){
+        list.lastElementChild.remove();
+    }
+    imagesToRender.forEach(image => {
+        const html = renderHornyAnimal(image);
+        const dom = htmlToDOM(html);
+        list.appendChild(dom);
+    });
+}
 
 images.forEach(image => {
 
@@ -14,30 +25,22 @@ images.forEach(image => {
     list.appendChild(dom);
 });
 
-
 imageTypeFilter.addEventListener('change', () => {
 
     const filterString = imageTypeFilter.value;
+    console.log(filterString);
     let filteredImages = null;
 
     if (!filterString) {
         filteredImages = images;
     } else {
         filteredImages = images.filter(image => {
+            console.log(image);
             return image.keyword === filterString; 
         });
     }
+    console.log(filteredImages);
     render(filteredImages);
 });
 
 render(images);
-function render(imagesToRender) {
-    while (URL.lastElementChild){
-        ul.lastElementChild.remove();
-    }
-    imagesToRender.forEach(image => {
-        const html = renderHornyAnimal(image);
-        const dom = htmlToDOM(html);
-        ul.appendChild(dom);
-    });
-}
